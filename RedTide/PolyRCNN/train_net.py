01659677992025a0ffb1d49c550663743ea5633b
Tianqi Hao <1534441778@qq.com>
2025-04-29 16:13:48 +0800

    htq

---
+++ RedTide/PolyRCNN/train_net.py
@@ -0,0 +1,175 @@
+"""
+PolyRCNN Training Script.
+"""
+
+import os
+import itertools
+import time
+from typing import Any, Dict, List, Set
+import logging
+from collections import OrderedDict
+
+import torch
+
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import build_detection_train_loader
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, launch
+from detectron2.evaluation import COCOEvaluator, verify_results
+from detectron2.solver.build import maybe_add_gradient_clipping
+
+from polyrcnn import PolyRCNNDatasetMapper, add_polyrcnn_config, PolyRCNNWithTTA
+
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.data.datasets.coco import load_coco_json
+
+
+class Trainer(DefaultTrainer):
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        """
+        构建评估器，也就是在模型训练完成之后，对模型性能进行评估的工具
+        """
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        return COCOEvaluator(dataset_name, cfg, True, output_folder)
+
+    @classmethod
+    def build_train_loader(cls, cfg):
+        mapper = PolyRCNNDatasetMapper(cfg, is_train=True)
+        return build_detection_train_loader(cfg, mapper=mapper)
+
+    @classmethod
+    def build_optimizer(cls, cfg, model):
+        params: List[Dict[str, Any]] = []
+        memo: Set[torch.nn.parameter.Parameter] = set()
+        for key, value in model.named_parameters(recurse=True):
+            if not value.requires_grad:
+                continue
+            # Avoid duplicating parameters
+            if value in memo:
+                continue
+            memo.add(value)
+            lr = cfg.SOLVER.BASE_LR
+            weight_decay = cfg.SOLVER.WEIGHT_DECAY
+            if "backbone" in key:
+                lr = lr * cfg.SOLVER.BACKBONE_MULTIPLIER
+            params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
+
+        def maybe_add_full_model_gradient_clipping(optim):  # optim: the optimizer class
+            # detectron2 doesn't have full model gradient clipping now
+            clip_norm_val = cfg.SOLVER.CLIP_GRADIENTS.CLIP_VALUE
+            enable = (
+                cfg.SOLVER.CLIP_GRADIENTS.ENABLED
+                and cfg.SOLVER.CLIP_GRADIENTS.CLIP_TYPE == "full_model"
+                and clip_norm_val > 0.0
+            )
+
+            class FullModelGradientClippingOptimizer(optim):
+                def step(self, closure=None):
+                    all_params = itertools.chain(*[x["params"] for x in self.param_groups])
+                    torch.nn.utils.clip_grad_norm_(all_params, clip_norm_val)
+                    super().step(closure=closure)
+
+            return FullModelGradientClippingOptimizer if enable else optim
+
+        optimizer_type = cfg.SOLVER.OPTIMIZER
+        if optimizer_type == "SGD":
+            optimizer = maybe_add_full_model_gradient_clipping(torch.optim.SGD)(
+                params, cfg.SOLVER.BASE_LR, momentum=cfg.SOLVER.MOMENTUM
+            )
+        elif optimizer_type == "ADAMW":
+            optimizer = maybe_add_full_model_gradient_clipping(torch.optim.AdamW)(
+                params, cfg.SOLVER.BASE_LR
+            )
+        else:
+            raise NotImplementedError(f"no optimizer type {optimizer_type}")
+        if not cfg.SOLVER.CLIP_GRADIENTS.CLIP_TYPE == "full_model":
+            optimizer = maybe_add_gradient_clipping(cfg, optimizer)
+        return optimizer
+
+    @classmethod
+    def test_with_TTA(cls, cfg, model):
+        print("---------------------------------")
+        print("---------------------------------")
+        logger = logging.getLogger("detectron2.trainer")
+        logger.info("Running inference with test-time augmentation ...")
+        model = PolyRCNNWithTTA(cfg, model)
+        evaluators = [
+            cls.build_evaluator(
+                cfg, name, output_folder=os.path.join(cfg.OUTPUT_DIR, "inference_TTA")
+            )
+            for name in cfg.DATASETS.TEST
+        ]
+        res = cls.test(cfg, model, evaluators)
+        res = OrderedDict({k + "_TTA": v for k, v in res.items()})
+        return res
+
+
+def setup(args):
+    """
+    添加基本配置到cfg
+    """
+    cfg = get_cfg()
+    add_polyrcnn_config(cfg)
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    cfg.freeze()
+    default_setup(cfg, args)
+    return cfg
+
+
+def register_my_dataset(dataset_name="aicrowd_train",
+                        TRAIN_JSON="../../data/train/annotation.json",
+                        TRAIN_PATH="../../data/train/images"):
+    """通过detectron2框架注册COCO格式的数据集.
+
+       usage::
+       from detectron2.data import DatasetCatalog, MetadataCatalog
+       from detectron2.data.datasets.coco import load_coco_json
+
+    :param TRAIN_JSON: 标注json文件
+    :param TRAIN_PATH: 图像文件夹路径
+    """
+    # CLASS_NAMES = []
+    DatasetCatalog.register(dataset_name, lambda: load_coco_json(TRAIN_JSON, TRAIN_PATH, dataset_name, ["cor_cls_poly"]))
+    # 原始的coco格式的文件里边键没有"cor_cls_poly"，所以这里要加上
+    MetadataCatalog.get(dataset_name).set(json_file=TRAIN_JSON, image_root=TRAIN_PATH)
+
+
+def main(args):
+    cfg = setup(args)
+    # Note: Modify the parameters in `register_my_dataset` for different datasets:
+    # - `dataset_name`: The name of the dataset to register.
+    # - `TRAIN_JSON`: Path to the COCO-format annotation file for the dataset.
+    # - `TRAIN_PATH`: Path to the folder containing the dataset images.
+    register_my_dataset(dataset_name="aicrowd_train",
+                        TRAIN_JSON="./data/train/annotation_preprocessed.json",
+                        TRAIN_PATH="./data/train/images")
+    if args.eval_only:
+        model = Trainer.build_model(cfg)
+        DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(cfg.MODEL.WEIGHTS, resume=args.resume)
+        res = Trainer.test(cfg, model)
+        if cfg.TEST.AUG.ENABLED:
+            res.update(Trainer.test_with_TTA(cfg, model))
+        if comm.is_main_process():
+            verify_results(cfg, res)
+        return res
+
+    trainer = Trainer(cfg)
+    trainer.resume_or_load(resume=True)
+    return trainer.train()
+
+
+if __name__ == "__main__":
+    args = default_argument_parser().parse_args()
+    print("Command Line Args:", args)
+    launch(
+        main,
+        args.num_gpus,
+        num_machines=args.num_machines,
+        machine_rank=args.machine_rank,
+        dist_url=args.dist_url,
+        args=(args,),
+    )
